8abb0099685bf392c772413274a2681245c90440
Thiago Paiva <paiva.thiago@gmail.com>
2019-07-03 22:55:54 -0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,70 @@
+HTMLElement.prototype.removeClass = function(remove) {
+    var newClassName = "";
+    var i;
+    var classes = this.className.split(" ");
+    for(i = 0; i < classes.length; i++) {
+        if(classes[i] !== remove) {
+            newClassName += classes[i] + " ";
+        }
+    }
+    this.className = newClassName;
+}
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let score = 0;
+function updateScore(somar){
+    score = somar ? score+2 : score - 1;     
+}
+function resetBoard(){
+    [hasFlippedCard,lockBoard,firstCard,secondCard]=[false,false,null,null];
+}
+function flipCard() {
+    if(!lockBoard){
+        if (this === firstCard) return;
+        this.classList.add('flip');
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+        }
+        secondCard=this;
+        if(firstCard!==secondCard) checkForMatch();
+    }
+}
+
+function checkForMatch(){
+    let confere = (firstCard.dataset.tipo === secondCard.dataset.tipo);
+    confere ? disableCards() : unflipCards();
+    if( document.querySelectorAll('.who').length==0){
+        alert(`FIM DE PARTIDA ${score} pontos`)
+    }
+}
+function disableCards() {
+    updateScore(true);
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    firstCard.removeClass('who');   
+    secondCard.removeClass('who');   
+    resetBoard();
+}
+function unflipCards() {
+    updateScore(false);
+    lockBoard=true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');  
+        resetBoard();
+    }, 1500);
+}
+ (function shuffle() {
+      cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+      });
+     })();
+cards.forEach(card => card.addEventListener('click', flipCard));
+document.getElementById('btnStart').addEventListener('click',()=>{document.getElementById('start').style.display='none';});
